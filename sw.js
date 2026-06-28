1f8160c6cf86f18aa15e1ed6cc8ad1211b70c712
sainivansh1708-ux <sainivansh1708@gmail.com>
2026-06-28 18:44:37 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE = 'jee2027-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'jee2027-v2';
+const ASSETS = [
+  '/jee2027/',
+  '/jee2027/index.html',
+  '/jee2027/manifest.json',
+  '/jee2027/icon-192.png',
+  '/jee2027/icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -15,6 +21,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/jee2027/index.html')))
   );
 });
